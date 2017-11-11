d60191f4ff62c14b231c3002d483b81bfbcc7dcc
Matt Twardowski <mttwardowski@gmail.com>
2017-11-11 14:35:28 -0500

    added mf_powered_by to apps

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171109202902) do
+ActiveRecord::Schema.define(version: 20171111171826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20171109202902) do
     t.string   "from_name"
     t.integer  "postmark_signature_id"
     t.integer  "user_id",               :foreign_key=>{:references=>"users", :name=>"fk_apps_user_id", :on_update=>:no_action, :on_delete=>:no_action}, :index=>{:name=>"fk__apps_user_id", :using=>:btree}
+    t.integer  "show_mf_powered"
   end
 
   create_table "email_templates", force: :cascade do |t|
