e4ce7de4c4b8e59879eca8758e159d5f90767332
Matt Twardowski <mttwardowski@gmail.com>
2018-02-25 13:45:03 -0500

    Added Forgot Password Template and Route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,59 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
 
+
+  # POST ROUTE
+  # ----------
+  # Retrieves user's password and sends them an email
+  # with their password
+  #
+  # PARAMETERS
+  # ----------
+  # email: Email Address of the User
+  #
+  def send_reset_password_email
+
+
+    @user = User.where(:email => params[:email]).first
+
+    if @user
+
+      html = File.open("app/views/email/password_reset.html.erb").read
+      @renderedhtml = "1"
+      ERB.new(html, 0, "", "@renderedhtml").result(binding)
+
+      client = Postmark::ApiClient.new('b650bfe2-d2c6-4714-aa2d-e148e1313e37', http_open_timeout: 60)
+      response = client.deliver(
+          :subject => "MailFunnels Password Reset",
+          :to => @user.email,
+          :from => 'MailFunnels noreply@custprotection.com',
+          :html_body => @renderedhtml,
+          :track_opens => 'true')
+
+      response = {
+          success: true,
+          message: 'Your password has been sent to your email..'
+      }
+
+      render json: response
+
+    else
+      puts "final else"
+      # Return Error Response
+      response = {
+          success: false,
+          message: 'Sorry, we could not find an account with this email.'
+      }
+
+      render json: response
+
+    end
+
+
+  end
+
+
+
   # GET /users
   def index
     @users = User.all
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ MailFunnelServer::Application.routes.draw do
   # Unsubscribe Page Route
   get '/unsubscribe/:subscriber_id', to: 'subscribers#unsubscribe_page'
 
+  # User Routes
+  post '/send_reset_password_email' => 'users#send_reset_password_email'
+
   # Postmark Webhook Routes
   post '/email_opened' => 'email_jobs#email_opened_hook'
   post '/email_clicked' => 'email_jobs#email_clicked_hook'
